414bd333fe03ecb79ddb0e7a7d985d0bb2c937f3
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-08-24 11:43:30 +0800

    better check

--- Main.cs
+++ Main.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System.Diagnostics;
 using System.Management;
+using System.Security.Cryptography;
 
 namespace YuukiPS_Launcher
 {
@@ -87,6 +88,27 @@ namespace YuukiPS_Launcher
             return foldPath;
         }
 
+        // Check MD5
+        private static string CalculateMD5(string filename)
+        {
+            try
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(filename))
+                    {
+                        var hash = md5.ComputeHash(stream);
+                        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return "uk";
+            }
+
+        }
+
         [Obsolete]
         private void btStart_Click(object sender, EventArgs e)
         {
@@ -131,13 +153,22 @@ namespace YuukiPS_Launcher
 
             // Check Game Path
             string FullGamePath;
+            string MetadataPath;
+            string Metadata_md5_api_ori;
+            string Metadata_md5_api_patch;
             if (File.Exists(cn))
             {
                 FullGamePath = cn;
+                MetadataPath = Path.Combine(Folder_Game_Now, "YuanShen_Data", "Managed", "Metadata");
+                Metadata_md5_api_ori = "idk";
+                Metadata_md5_api_patch = "";
             }
             else if (File.Exists(os))
             {
                 FullGamePath = os;
+                MetadataPath = Path.Combine(Folder_Game_Now, "GenshinImpact_Data", "Managed", "Metadata");
+                Metadata_md5_api_ori = "809de2b9cd7a0f8cdd8687e3a8291cbb";
+                Metadata_md5_api_patch = "6edd177848f05b0f9200a033aac8479c";
             }
             else
             {
@@ -145,8 +176,86 @@ namespace YuukiPS_Launcher
                 return;
             }
 
-            Console.WriteLine("Game Path: " + FullGamePath);
+            Console.WriteLine("Folder MetadataPath: " + MetadataPath);
+            Console.WriteLine("File Game: " + FullGamePath);
 
+            // Check Metadata file
+            string Medata_now = Path.Combine(MetadataPath, "global-metadata.dat");
+            if (!File.Exists(Medata_now))
+            {
+                MessageBox.Show("Metadata file not found");
+                return;
+            }
+
+            string Metadata_now_md5 = CalculateMD5(Medata_now);
+
+            Console.WriteLine("MetadataNow: " + Metadata_now_md5);
+
+            string metadata_patch_path = Path.Combine(MetadataPath, "global-metadata-patch.dat");
+            string metadata_ori_path = Path.Combine(MetadataPath, "global-metadata-ori.dat");
+
+            string metadata_patch_path_md5 = CalculateMD5(metadata_patch_path);
+            string metadata_ori_path_md5 = CalculateMD5(metadata_ori_path);
+
+            Console.WriteLine("MetadataPatch: " + metadata_patch_path_md5);
+            Console.WriteLine("MetadataORI: " + metadata_ori_path_md5);
+
+            // check if found patch just skip it
+            if (progress == null)
+            {
+                if (Metadata_md5_api_patch != Metadata_now_md5)
+                {
+                    Console.WriteLine("Metadata doesn't match, patch it...");
+
+                    if (!File.Exists(metadata_ori_path))
+                    {
+                        // JIK API ORI SAMA DENGAN SEKARANG, BACKUP
+                        if (Metadata_md5_api_ori == Metadata_now_md5)
+                        {
+                            Console.WriteLine("Backup Metadata ori...");
+                            File.Copy(Medata_now, metadata_ori_path, true);
+                        }
+                        else
+                        {
+                            // JIKA API ORI BEDA DENGAN META SEKARANG:
+                            Console.WriteLine("Original version backup failed because md5 doesn't match\r\n");
+                            return;
+                        }
+                    }
+
+                    if (!File.Exists(metadata_patch_path))
+                    {
+                        // JIKA FILE PATCH TIDAK ADA
+                        Console.WriteLine("No Found Patch file....");
+                        // TODO: add download
+                        return;
+                    }
+                    else
+                    {
+                        // JIKA PATCH ADA, CEK APAKAH API DAN MERA PATCH MD5 SAMA
+                        if (Metadata_md5_api_patch == metadata_patch_path_md5)
+                        {
+                            Console.WriteLine("Patch done...");
+                            File.Copy(metadata_patch_path, Medata_now, true);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Patch version failed because md5 doesn't match\r\n");
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    // skip
+                }
+            }
+            else
+            {
+                // skip
+            }
+
+            // For Proxy
             if (proxy == null)
             {
                 proxy = new ProxyController(set_proxy_port, set_server_host);
@@ -160,6 +269,10 @@ namespace YuukiPS_Launcher
                 btStart.Text = "Launch";
             }
 
+            // For Patch
+            String Metadata_Game = Folder_Game_Now;
+
+            // For Game
             if (progress == null)
             {
                 progress = new Process();
@@ -180,7 +293,41 @@ namespace YuukiPS_Launcher
             else
             {
                 Console.WriteLine("Game Stop!");
-                KillProcessAndChildrens(progress.Id);
+                try
+                {
+                    KillProcessAndChildrens(progress.Id);
+                }
+                catch (Exception exx)
+                {
+                    //skip
+                }
+
+                progress.WaitForExit();
+                progress = null;
+
+                if (File.Exists(metadata_ori_path))
+                {
+                    try
+                    {
+                        // JIKA API ORI TIDAK COCOK DENGAN METADATA SEKARANG
+                        if (Metadata_md5_api_ori != Metadata_now_md5)
+                        {
+                            Console.WriteLine("Revert to original version...");
+                            File.Copy(metadata_ori_path, Medata_now, true);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Because this is still using original version");
+                            return;
+                        }
+                    }
+                    catch (Exception xc)
+                    {
+                        Console.WriteLine("Failed to return to the original version because the game has not been closed");
+                    }
+
+                }
+
             }
 
         }
