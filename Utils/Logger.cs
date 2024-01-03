8e8131918f94fb365fecbbfac0dd44ed475cf490
Pikaki So <palitroki22@gmail.com>
2024-01-04 00:10:54 +0100

    Fix my terrible code

--- Utils/Logger.cs
+++ Utils/Logger.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace YuukiPS_Launcher.Utils
 {
     internal class Logger
     {
+        public static string logPath = "";
+        public static string strMsg = "";
+
+        public void initLogging(string startMsg, string lPath)
+        {
+            logPath = lPath;
+            strMsg = startMsg;
+
+            File.WriteAllText(logPath, strMsg);
+        }
+
         public static void Info(string type, string msg)
         {
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("[");
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.Write(DateTime.Today.ToString("hh:mm:ss"));
+            Console.Write(DateTime.Now.ToString("hh:mm:ss"));
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("] ");
             Console.Write("[");
@@ -28,6 +41,8 @@ namespace YuukiPS_Launcher.Utils
             Console.Write("] ");
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.Write(msg + "\n");
+
+            File.AppendAllText(logPath, $"[${DateTime.Now.ToString("hh:mm:ss")}] [${type}:INFO] "+msg+"\n");
         }
 
         public static void Error(string type, string msg)
@@ -35,7 +50,7 @@ namespace YuukiPS_Launcher.Utils
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("[");
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.Write(DateTime.Today.ToString("hh:mm:ss"));
+            Console.Write(DateTime.Now.ToString("hh:mm:ss"));
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("] ");
             Console.ForegroundColor = ConsoleColor.White;
@@ -50,13 +65,15 @@ namespace YuukiPS_Launcher.Utils
             Console.Write("] ");
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.Write(msg + "\n");
+
+            File.WriteAllText(logPath, $"[${DateTime.Now.ToString("hh:mm:ss")}] [${type}:ERROR] " + msg + "\n");
         }
         public static void Warning(string type, string msg)
         {
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("[");
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.Write(DateTime.Today.ToString("hh:mm:ss"));
+            Console.Write(DateTime.Now.ToString("hh:mm:ss"));
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("] ");
             Console.ForegroundColor = ConsoleColor.White;
@@ -71,6 +88,7 @@ namespace YuukiPS_Launcher.Utils
             Console.Write("] ");
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.Write(msg + "\n");
+            File.WriteAllText(logPath, $"[${DateTime.Now.ToString("hh:mm:ss")}] [${type}:WARNING] " + msg + "\n");
         }
     }
 }
